ea81c732922b1cfc91e94fd552c59b2f9d5e9a3d
Anton Rijkers <antonrijkers@mac.kpn>
2025-01-13 14:09:31 +0100

    Changed height sizedbox

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,7 +14,7 @@ class _HomepageState extends State<MyHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: 200),
+            SizedBox(height: 250),
             Center(
               child: Text(
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor, eros in ultricies tempus, nunc mauris laoreet sapien, vel facilisis metus nulla id urna. Donec congue nisi eu nisl pretium, eget eleifend urna sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque in ultrices turpis. Integer feugiat risus eu justo tincidunt, a scelerisque libero hendrerit. Vivamus scelerisque, sapien vel tincidunt dapibus, nisi justo auctor orci, vel ultricies elit ligula non metus.'),
