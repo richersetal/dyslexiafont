618ef752e789e2f454c8fa77b934366a97c1fc8a
Anton Rijkers <antonrijkers@mac.kpn>
2025-01-13 14:02:31 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/com_richersetal_dyslexiafont/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.com_richersetal_dyslexiafont
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 200),
+            Center(
+              child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor, eros in ultricies tempus, nunc mauris laoreet sapien, vel facilisis metus nulla id urna. Donec congue nisi eu nisl pretium, eget eleifend urna sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque in ultrices turpis. Integer feugiat risus eu justo tincidunt, a scelerisque libero hendrerit. Vivamus scelerisque, sapien vel tincidunt dapibus, nisi justo auctor orci, vel ultricies elit ligula non metus.'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
