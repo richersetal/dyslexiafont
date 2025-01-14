783cb510527ad9ae05b3d1b17e3052ee58ad9ebe
Anton Rijkers <antonrijkers@mac.kpn>
2025-01-14 12:19:23 +0100

    mappenstructuur aangepast

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -28,7 +28,7 @@ class _HomepageState extends State<MyHomePage> {
                 SizedBox(height: 10),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); // Sluit de dialoog
+                    Navigator.of(context).pop();
                   },
                   child: Text('Sluit'),
                 ),
@@ -51,11 +51,11 @@ class _HomepageState extends State<MyHomePage> {
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Text(
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor, eros in ultricies tempus, nunc mauris laoreet sapien, vel facilisis metus nulla id urna. Donec congue nisi eu nisl pretium, eget eleifend urna sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque in ultrices turpis. Integer feugiat risus eu justo tincidunt, a scelerisque libero hendrerit. Vivamus scelerisque, sapien vel tincidunt dapibus, nisi justo auctor orci, vel ultricies elit ligula non metus.',
+                style: TextStyle(fontFamily: 'Lexens'),
                 textAlign: TextAlign.center,
               ),
             ),
             SizedBox(height: 20),
-            // Knop om de pop-up te tonen
             ElevatedButton(
               onPressed: () {
                 _showCustomDialog(context);
